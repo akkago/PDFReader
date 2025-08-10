6b8ae43969331db870036a8aa81f2d8b08fbd2bd
ryabov-si <102360705+akkago@users.noreply.github.com>
2025-08-10 16:00:09 +0300

    fix

--- server/parsePageContent.js
+++ server/parsePageContent.js
@@ -44,22 +44,54 @@ function parsePageContent(pageContents) {
 
   const otchetnost = [];
   const dates = ['2024-09-30', '2023-12-31', '2022-12-31'];
+  const codeData = [];
   
-  for (const line of allLines) {
-    const codeMatch = line.match(/^(\d{4,5})\s*$/);
-    if (codeMatch) {
-      const code = codeMatch[1];
-      if (code.length === 4 && parseInt(code) >= 1900 && parseInt(code) <= 2100) {
-        continue;
+  for (let i = 0; i < allLines.length; i++) {
+    const line = allLines[i];
+    
+    const codeMatches = line.match(/\b(\d{4,5})\b/g);
+    if (codeMatches) {
+      for (const codeMatch of codeMatches) {
+        const code = codeMatch;
+        if (code.length === 4 && parseInt(code) >= 1900 && parseInt(code) <= 2100) {
+          continue;
+        }
+        
+        const sums = [];
+        let j = i + 1;
+        
+        while (j < allLines.length && j < i + 10) {
+          const nextLine = allLines[j];
+          const sumMatch = nextLine.match(/^(\d[\d\s]*)$/);
+          
+          if (sumMatch) {
+            const sumStr = sumMatch[1].replace(/\s/g, '');
+            const sum = parseInt(sumStr);
+            if (!isNaN(sum) && sum > 0) {
+              sums.push(sum);
+            }
+          } else if (nextLine.match(/\b\d{4,5}\b/)) {
+            break;
+          }
+          j++;
+        }
+        
+        if (sums.length > 0) {
+          codeData.push({ code, sums });
+        } else {
+          codeData.push({ code, sums: [0, 0, 0] });
+        }
       }
-      
-      for (const date of dates) {
-        otchetnost.push({
-          date: date,
-          code: code,
-          sum: 0
-        });
-      }
+    }
+  }
+  
+  for (const { code, sums } of codeData) {
+    for (let i = 0; i < dates.length; i++) {
+      otchetnost.push({
+        date: dates[i],
+        code: code,
+        sum: sums[i] || 0
+      });
     }
   }
   
